10b8c323972e4f885eb65c4349043f8419ae7cbb
ecollins27 <ehren.collins@gmail.com>
2025-10-20 19:36:59 -0400

    Added os config Added task scheduler set to run 3AM Friday mornings

--- main.py
+++ main.py
@@ -8,14 +8,28 @@ import select
 
 import os_config
 import server_config
+import schedule
+import time
+import datetime
 
 logger = logging.getLogger(__name__)
 
 SCRIPT_PATH = os.path.abspath(os.path.dirname(__file__))
 DEFAULT_SERVER_CFG_PATH = os.path.join(SCRIPT_PATH, "server_config.cfg")
 DEFAULT_OS_CFG_PATH = os.path.join(SCRIPT_PATH, "os_config.cfg")
+DEFAULT_LOG_PATH = os.path.join(SCRIPT_PATH, "update.log")
 server_cfg = server_config.load_config(DEFAULT_SERVER_CFG_PATH)
 os_cfg = os_config.load_config(DEFAULT_OS_CFG_PATH)
+
+def makeLogger(logFile):
+    formatter = logging.Formatter(fmt='[%(asctime)s] %(levelname)-8s %(message)s',
+                                  datefmt='%Y-%m-%d %H:%M:%S')
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
+    fh = logging.FileHandler(logFile)
+    fh.setFormatter(formatter)
+    logger.addHandler(fh)
+    return logger
 
 def update_libraries(client: paramiko.SSHClient, password, command: str):
     try:
@@ -55,16 +69,45 @@ def update_server(server: server_config.Server, os: os_config.OS):
     client = paramiko.SSHClient()
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     try:
-        logger.info(f"Attempting SSH connection to {ip}")
         client.connect(ip, username=server.username,password=server.password)
     except Exception as e:
         return f'Unable to SSH into {ip}'
     update_output = f'{server.name}\n'
-    update_output += f'    {update_libraries(client, server.password, os.lib_update)}\n'
-    update_output += f'    {check_distro(client, server.password, os.distro_check, os.distro_check_pos, os.distro_check_neg)}\n'
+    update_output += f'  - {update_libraries(client, server.password, os.lib_update)}\n'
+    update_output += f'  - {check_distro(client, server.password, os.distro_check, os.distro_check_pos, os.distro_check_neg)}\n'
+    try:
+        client.close()
+    except Exception as e:
+        pass
+    logger.info(f"{ip} updated")
     return update_output
 
+def notify_discord(update_info):
+    try:
+        client = paramiko.SSHClient()
+        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        client.connect(server_cfg.nauticock.ip, username=server_cfg.nauticock.username, password=server_cfg.nauticock.password, timeout=30)
+        client.exec_command(server_cfg.nauticock.command + f" --text \"AutoUpdater updated the following servers:\" --mono \"{update_info}\"")
+        client.close()
+        logger.info("Report to sent to Discord")
+    except Exception as e:
+        logger.error("Unable to connect to notify NauticockBot")
+
+def update_all_servers():
+    logger.info(f"Server updates started at {datetime.datetime.now()}")
+    update_info = ''
+    for server in server_cfg.servers:
+        update_info += update_server(server, os_cfg.os_dict[server.os])
+    notify_discord(update_info)
+    logger.info(f"Server updates finished at {datetime.datetime.now()}")
 
 if __name__ == '__main__':
-    for server in server_cfg.servers:
-        print(update_server(server, os_cfg.os_dict[server.os]))
+    logger = makeLogger(DEFAULT_LOG_PATH)
+    # update_all_servers()
+    try:
+        schedule.every().friday.at("03:00").do(update_all_servers)
+        while True:
+            schedule.run_pending()
+            time.sleep(60)
+    except Exception as e:
+        logger.error("A fatal error has occurred")
--- server_config.py
+++ server_config.py
@@ -4,6 +4,13 @@ import configparser
 from dataclasses import dataclass
 
 @dataclass
+class Nauticock:
+    username: str
+    password: str
+    ip: str
+    command: str
+
+@dataclass
 class Server:
     name: str
     ip: str
@@ -13,6 +20,7 @@ class Server:
 
 @dataclass
 class ServerConfig:
+    nauticock: Nauticock
     servers: list[Server]
 
 def load_config(config_path: os.PathLike):
@@ -23,11 +31,15 @@ def load_config(config_path: os.PathLike):
         print(f"Failed to load config file from {config_path}: {e}", file=sys.stderr)
         exit(1)
     try:
+        nauticock = Nauticock(username=cfg.get("nauticock", "username"), password=cfg.get("nauticock", "password"),
+                              ip=cfg.get("nauticock", "ip"), command=cfg.get("nauticock", "command"))
         servers = []
         for section in cfg.sections():
+            if section == 'nauticock':
+                continue
             server = Server(name=section, ip=cfg.get(section, 'ip'), username=cfg.get(section, 'username'), password=cfg.get(section, 'password'), os=cfg.get(section, 'os'))
             servers.append(server)
-        config = ServerConfig(servers=servers)
+        config = ServerConfig(nauticock=nauticock, servers=servers)
     except Exception as e:
         print(f"Error in config file {config_path}: {e}", file=sys.stderr)
         exit(1)
